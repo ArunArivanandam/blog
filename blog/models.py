cc8782b045c93c0297e1b95beac6f23ee0daacac
ArunArivanandam <88358701+ArunArivanandam@users.noreply.github.com>
2023-04-09 13:06:46 +0530

    basic functionality

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+        .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -17,6 +23,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                             choices=Status.choices,
                             default=Status.DRAFT)
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     class Meta:
         ordering = ['-publish']
